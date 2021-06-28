8909096821726ac06138691ba67cb69093ace427
MikeGer <mikeman@inbox.ru>
2021-06-28 16:59:27 +0300

    added base_teplate for orderapp

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 app_name="ordersapp"
 
 urlpatterns = [
-    path
+    path('', ordersapp.OrderList, name='orders_list'),
+    path('create/<pk>', ordersapp.OrderCreate.as_view(), name='create'),
+    path('update/<pk>', ordersapp.OrderUpdate.as_view(), name='update'),
+    path('delete/<pk>', ordersapp.OrderDelete.as_view(), name='delete'),
+    path('read/<pk>', ordersapp.OrderRead.as_view(), name='read'),
+    path('forming/complete/<pk>', ordersapp.forming_complete, name='forming_complete'),
 ]
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
+
+class OrderList(ListView):
+    pass
+
+
+class OrderCreate(CreateView):
+    pass
+
+
+class OrderUpdate(UpdateView):
+    pass
+
+
+class OrderDelete(DeleteView):
+    pass
+
+
+class OrderRead(DetailView):
+    pass
+
+
+def forming_complete(request, pk):
+    pass
