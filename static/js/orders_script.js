24083c5b39f3432870ea44db62c7476e723dbac0
MikeGer <mikeman@inbox.ru>
2021-07-05 20:35:23 +0300

    added jquery.formset.js

--- static/js/orders_script.js
+++ static/js/orders_script.js
@@ -48,4 +48,18 @@ window.onload = function () {
             $('.order_total_cost').text(order_total_price.toString());
     }
 
+    $('.formset_row').formset({
+       addText: 'добавить продукт',
+       deleteText: 'удалить',
+       prefix: 'orderitems',
+       removed: deleteOrderItem
+    });
+    function deleteOrderItem(row) {
+       var target_name= row[0].querySelector('input[type="number"]').name;
+       orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+       delta_quantity = -quantity_arr[orderitem_num];
+       orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    }
+
+
 };
