063fcf19689e79f8185c7e75c50e75bbd76b45c4
MikeGer <mikeman@inbox.ru>
2021-09-10 11:00:30 +0300

    added export products

--- adminapp/views.py
+++ adminapp/views.py
@@ -277,12 +277,12 @@ def export_users_xls(request):
     wb.save(response)
     return response
 
-def export_products_xls(request):
+def new_export_products_xls(request):
     response = HttpResponse(content_type='application/ms-excel')
     response['Content-Disposition'] = 'attachment; filename="products.xls"'
 
     wb = xlwt.Workbook(encoding='utf-8')
-    ws = wb.add_sheet('Products')
+    ws = wb.add_sheet('all_roducts')
 
     # Sheet header, first row
     row_num = 0
@@ -290,19 +290,31 @@ def export_products_xls(request):
     font_style = xlwt.XFStyle()
     font_style.font.bold = True
 
-    columns = ['id', 'name', 'category', 'price' ]
+    columns = ['id', 'name', 'price', 'category_name' ]
+    id = Product.objects.all().values_list('id')
+    name = Product.objects.all().values_list('name')
+    category_name = ProductCategory.objects.all().values_list('name')
+    price = Product.objects.all().values_list('price')
+
+    row_data = [
+        id,
+        name,
+        category_name,
+        price
+    ]
 
     for col_num in range(len(columns)):
-        ws.write(row_num, col_num, columns[col_num], font_style)
+        # ws.write(row_num, col_num, columns[col_num], font_style)
+        ws.write(row_num, col_num, row_data, font_style)
 
     # Sheet body, remaining rows
-    font_style = xlwt.XFStyle()
-    cn = 'category'
-    rows = Product.objects.all().values_list('id', 'name', 'category', 'price')
-    for row in rows:
-        row_num += 1
-        for col_num in range(len(row)):
-            ws.write(row_num, col_num, row[col_num], font_style)
+    # font_style = xlwt.XFStyle()
+    # # cn = 'category'
+    # rows = Product.objects.all().values_list('id', 'name', 'price' , 'ProductCategory__name')
+    # for row in rows:
+    #     row_num += 1
+    #     for col_num in range(len(row)):
+    #         ws.write(row_num, col_num, row[col_num], font_style)
 
     wb.save(response)
     return response
